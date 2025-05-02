42641e4f623ba245ae050586bd0047174c51e0fe
Sedeno Antonio <YCG8L6@ad.insee.intra>
2025-05-02 20:07:12 +0200

    commit 4 : modif arborescence

--- prog_python/ProgPy_06_CreationJSON.py
+++ prog_python/ProgPy_06_CreationJSON.py
@@ -15,23 +15,23 @@ print("cheminWork  : "+cheminWork)
 cheminWork2 = cheminWork.replace('\\','/').replace('/prog_python','')
 print("cheminWork2 : "+cheminWork2)
 
-rmtree(cheminWork2+"/json")
-mkdir(cheminWork2+"/json")
+rmtree(cheminWork2+"/site/json")
+mkdir(cheminWork2+"/site/json")
 
 # lecture des dataframes puis transformation en JSON...
 json_df_listFS = read_csv(cheminWork2+"/dataframe/df_listFS_C.csv",sep=";").to_json(orient="records")
 # print(json_df_listFS)
-with open(cheminWork2+"/json/df_listFS.json", "w") as outfile:
+with open(cheminWork2+"/site/json/df_listFS.json", "w") as outfile:
     outfile.write(json_df_listFS)
 
 json_df_listVV = read_csv(cheminWork2+"/dataframe/df_listVV.csv",sep=";")[:].to_json(orient="records")
 # print(json_df_listVV)
-with open(cheminWork2+"/json/df_listVV.json", "w") as outfile:
+with open(cheminWork2+"/site/json/df_listVV.json", "w") as outfile:
     outfile.write(json_df_listVV)
 
 json_df_fusion = read_csv(cheminWork2+"/dataframe/df_fusion.csv",sep=";").to_json(orient="records")
 # print(json_df_fusion)
-with open(cheminWork2+"/json/df_fusion.json", "w") as outfile:
+with open(cheminWork2+"/site/json/df_fusion.json", "w") as outfile:
     outfile.write(json_df_fusion)
 
 print()
