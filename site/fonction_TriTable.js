646588928116b34943f5cb4760e7f70d73e91340
Sedeno Antonio <YCG8L6@ad.insee.intra>
2025-05-05 17:48:09 +0200

    commit16 : essai filtering

--- site/fonction_TriTable.js
+++ site/fonction_TriTable.js
@@ -3,7 +3,7 @@ function sortTable(n) {
 
     let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById('MaTable');
-    // console.log(table.rows[0]);
+    // console.log("ligne :"+table.rows[0]);
 
     switching = true;
     dir = "asc";
@@ -22,7 +22,9 @@ function sortTable(n) {
         /* Get the two elements you want to compare, one from current row and one from the next: */
             x = rows[i].getElementsByTagName("td")[n];
             y = rows[i + 1].getElementsByTagName("td")[n];
-            
+            console.log("x = "+x.innerHTML);
+            console.log("y = "+y.innerHTML);
+
         /* Check if the 2 rows should switch place, based on the direction, asc or desc: */
             console.log("num colonne = "+n);
             if (dir == "asc") {
@@ -62,12 +64,43 @@ function sortTable(n) {
             switching = true;
         // Each time a switch is done, increase this count by 1:
             switchcount ++;
+            console.log("comptage inversion : "+switchcount)
         } else {
         /* If no switching has been done & dir is "asc", set dir to "desc" and run the while loop again. */
             if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
                 switching = true;
+                dir = "desc";
             }
         }
     }
 }
+
+
+// Algorithme QuickSort
+function sort2Table(n) {
+    let table, rows;
+    table = document.getElementById('MaTable');
+    rows = table.rows;
+
+    // Base case: If the array has one or no elements, it is already sorted.
+    if (rows.length <= 1) return rows;
+  
+    // Choosing the first element in the array as the pivot.
+    const pivot = rows[0].getElementsByTagName("td")[n].innerHTML;
+    console.log("pivot : "+pivot);
+    // Creating two empty arrays to store elements less than (left) and greater than (right) the pivot.
+    const left = [];
+    const right = [];
+  
+    // Looping through the array, starting from the second element because the first is the pivot.
+    for (let i = 1; i < rows.length; i++) {
+      // If the current element is smaller than the pivot, push it to the 'left' array.
+      if (rows[i].getElementsByTagName("td")[n].innerHTML < pivot) left.push(rows[i].getElementsByTagName("td")[n].innerHTML);
+      // If the current element is greater than or equal to the pivot, push it to the 'right' array.
+      else right.push(rows[i].getElementsByTagName("td")[n].innerHTML);
+    }
+  
+    // Concatenate the result of recursively sorting the 'left' array, the pivot, and then the 'right' array.
+    // Spread syntax '...' is used to concatenate arrays.
+    return [...sort2Table(left), pivot, ...sort2Table(right)];
+  }
